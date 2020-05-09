a55f1325c18b40e05dded087ae2f3735cea20b93
Andrew Greer <62513150+andrewpaulgreer@users.noreply.github.com>
2020-05-09 12:06:47 -0400

    this is my commit with the working timeblock colors, and the local storage. I had to move the local storage into the for loop to get it to work

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # jQuery-Day-Planner
+
+here is my completed jquery day planner. Max gave me a pretty good base to work with working through the text area id, and giving each save button a value that is equivilent with military time. 
+
+I initially had some trouble with getting my local storage to work, since it was not in the for loop, but after some research and  testing, I moved the getItem into my loop, so that it will recognize each input, and fiexed my problem. I sort of reverse engineered what i had based off of what I had seen in manipulating my text area ID and my save buttons, and used that throughout this project as I was told that this problem can be solved in many less lines of code that originally inteded after talking with my TA. 
--- assets/script.js
+++ assets/script.js
@@ -5,33 +5,37 @@ var currentHour = new Date().getHours();
 
 // onclick for the save buttons (several buttons, with the same class)
 $(".btn").on("click", function(){
-var time = $(this).val()
+var time = $(this).val() // declaring a variable for this.val
 
 var currentStorage = $("#textArea" + time).val().trim()
-// localStorage.setItem("#textArea", storeContent);
+
 localStorage.setItem("miltime" + time, currentStorage)
 });
 
-// function matchHours to work with the for loop to help determine the time and asscoiated color (currently Broken)
 
-// attempting to set local storage as well. 
-function matchHours (){
-var userInput = localStorage.getItem("miltime"+i)
-if(userInput != null){
-    $("#textArea"+i).val(userInput);
-}
+
+function matchHours(){
 for (var i = 9; i < 18; i++){
-   
+    // moved Local Sorage into the for loop since it will need to retrieve each of the values, and the loop will create that umbrella
+    var userInput = localStorage.getItem("miltime"+i)
+    if(userInput != null){
+        $("#textArea"+i).val(userInput);
+    }
+    // inserting currentHour in an if statement, and our time is less than the currenthour, then we will assign past (gray block)
     if (i < currentHour) {
     $("#textArea"+ i).addClass("past")
+      // else if statement, and if our current time parsed into an integer is eaqual to the currenthour, then we will assign it the present(Red), and remove past. 
 } else if (parseInt(currentHour) === i){
-    $("#textArea"+ i).removeClass("past").addClass("present")
-    
-} else (i > currentHour)
-    $("#textArea"+ i).removeClass("present").removeClass("past").addClass("future")
-    
+    $("#textArea"+ i).addClass("present")
+    $("#textArea"+ i).removeClass("past")
+    // anyhting else will be assgned future (green), and will remove the other two classes. 
+} else {
+    $("#textArea"+ i).addClass("future")
+    $("#textArea"+ i).removeClass("present past")
 }
-
+}
+}
+matchHours();
 // - Wait for DOM to be "ready", then...
 
    
@@ -49,8 +53,7 @@ for (var i = 9; i < 18; i++){
 // //         - Remote the "past" class from this time block
 // //         - Remove the "present" class from this time block
 // //         - Add the "future" class to this time block
-// }
-// updateHours();
+
     
 
 //   - Call the function we just created to update the hours the first time
